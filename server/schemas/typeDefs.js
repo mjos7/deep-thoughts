03107c6a6b66d554a967487c9df0759d9a595f90
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-10-23 21:15:59 -0700

    graphql mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,3 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
@@ -27,13 +26,26 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
